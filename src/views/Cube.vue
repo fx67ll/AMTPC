828f062b0d86ddd91b59ea2e853acd4cf84dff48
fx67ll <2717745014@qq.com>
2021-12-01 14:01:44 +0800

    format

--- src/views/Cube.vue
+++ src/views/Cube.vue
@@ -1,8 +1,33 @@
 <template>
+	<div class="cube-box">
+
+	</div>
 </template>
 
 <script>
+	import * as THREE from 'three/build/three.module.js';
+	import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+	import {
+		OrbitControls
+	} from 'three/examples/jsm/controls/OrbitControls.js';
+	import {
+		FBXLoader
+	} from 'three/examples/jsm/loaders/FBXLoader.js';
+	
+	export default {
+		name: "cube",
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
 </script>
 
-<style>
+<style lang="less" scoped="scoped">
+
 </style>
